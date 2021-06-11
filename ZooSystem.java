a4a81681427289994c147c67e8337e7c17ef9963
natalie whalen <nwhalen2@nd.edu>
2021-06-10 22:03:17 -0400

    abstract errors

--- ZooSystem.java
+++ ZooSystem.java
@@ -13,7 +13,9 @@ abstract class Animal{
 		this.bloodType = blood;
 	}
 
-	abstract reproduce() {}
+	abstract String reproduce();
+
+	abstract String eat();
 
 	String getName() { return this.name; }
 	void setName(String name) { this.name = name; }
@@ -24,7 +26,77 @@ abstract class Animal{
 	String getFoodType() { return this.foodType; }
 	void setFoodType(String food) { this.foodType = food; } 
 
-	String getBloodType() { return this.foodType; }
-	void setBloodType(String blood) { this.foodType = food; }	
+	String getBloodType() { return this.bloodType; }
+	void setBloodType(String blood) { this.bloodType = blood; }	
+
+}
+
+class Mammal extends Animal {
+
+	Mammal(String name, int weight, String food, String blood) {
+		super(name, weight, food, blood);
+	}
+
+	String reproduce() {
+		return "vivaparous";
+	}
+	
+//	abstract String eat();
+
+	String complexBrain() {
+		return "has a complex brain";
+	}
+
+}
+
+class Bird extends Animal {
+
+	boolean fly;
+	int wingSpan;
+
+	Bird(String name, int weight, String food, String blood) {
+		super(name, weight, food, blood);
+	}
+
+	String reproduce() {
+		return "ovoviviparous";
+	}
+
+//	abstract String eat();
+
+	boolean getFly() { return this.fly;	}
+	void setFly(boolean fly) { this.fly = fly; }
+
+	int getWingSpan() { return this.wingSpan; }
+	void setWingSpan(int wing) { this.wingSpan = wing; }
+
+}
+
+class Reptile extends Animal {
+
+	boolean hasScales;
+	boolean hasLegs;
+	
+	Reptile(String name, int weight, String food, String blood) {
+		super(name, weight, food, blood);
+	}
+/*
+	abstract String reproduce();
+	abstract String eat();
+*/
+	boolean getScales() { return this.hasScales; }
+	void setScales(boolean scales) { this.hasScales = scales; }
+
+	boolean getLegs() { return this.hasLegs; }
+	void setLegs(boolean legs) { this.hasLegs = legs; }
+
+}
+
+public class ZooSystem {
+
+	public static void main(String[] args) {
+		Reptile r1 = new Reptile("fred", 60, "omnivore", "cold-blooded");
+		System.out.println(r1.getName() + " weighs " + r1.getWeight());
+	}
 
 }
