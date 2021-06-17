0d7e67228d34a6e7354c896a76525bb6b663dd91
natalie whalen <nwhalen2@nd.edu>
2021-06-17 11:45:56 -0400

    added can/get/setSwim

--- Mammal.java
+++ Mammal.java
@@ -1,6 +1,7 @@
 public abstract class Mammal extends Animal {
 	final String bloodType = "warm-blooded";
 	final String foodType = foodTypeArray[1];
+	Boolean canSwim;
 
 	Mammal(String name, int weight, String faveFood) {
 		super(name, weight, faveFood);
@@ -13,5 +14,8 @@ public abstract class Mammal extends Animal {
 	final String reproduce() {
 		return "viviparous";
 	}
+
+	Boolean getSwim() { return this.canSwim; }
+	void setSwim(Boolean canSwim) { this.canSwim = canSwim; }
 
 }
